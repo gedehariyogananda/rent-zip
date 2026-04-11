b8ed34dc4fce521dfcf4ba5533012d119191d85b
gedehariyogananda <guary060504@gmail.com>
2026-04-12 03:19:08 +0700

    fix clean migration and fix seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -9,10 +9,11 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->call([
-            RoleSeeder::class,        // 1. roles: admin, member
-            CategorySeeder::class,    // 2. categories: pemasukan + pengeluaran + kostum cosplay
-            UserSeeder::class,        // 3. users + profiles (admin + 10 member)
-            CostumSeeder::class,      // 4. kostum cosplay Jepang
+            RoleSeeder::class, // 1. roles: admin, member
+            CategorySeeder::class, // 2. categories: pemasukan + pengeluaran + kostum cosplay
+            UserSeeder::class, // 3. users + profiles (admin + 10 member)
+            CostumSeeder::class, // 4. kostum cosplay Jepang
+            EventSeeder::class,
             // NarutoOrderSeeder::class, // 5. transaksi naruto (confirmated + pending + cart + rating prompt)
             // MaintenanceSeeder::class, // 6. maintenance kostum pasca pengembalian
             // FinanceSeeder::class,     // 7. finance: pemasukan dari order confirmated + pengeluaran operasional
